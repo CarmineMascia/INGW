e1f99535d240e4cc10a5fd3b3546b2ea93d49b60
Luigi Penza <91316353+Hex-adecimal@users.noreply.github.com>
2023-09-08 18:19:59 +0200

    Delete 3 - Codice Sorgente sviluppato/Ratatouille/statisticsApp directory

--- 3 - Codice Sorgente sviluppato/Ratatouille/statisticsApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatisticsappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'statisticsApp'
--- 3 - Codice Sorgente sviluppato/Ratatouille/statisticsApp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from statisticsApp.models import Receipt
-
-class ReceiptSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Receipt
-        fields = ('id', 'value', 'date')
